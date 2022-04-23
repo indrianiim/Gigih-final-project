8b7d596dbfc92eb6b94cf927ea279852d614f16d
Indrianiim <agnesia.indriani@gmail.com>
2022-04-23 10:12:11 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'catreings/index'
-  get 'catreings/show'
-  get 'catreings/new'
-  get 'catreings/edit'
+  resources :menus
+    resources :caterings
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
